04296800323307e1b85e70d242c18c4c83967be6
algo-fri-thm-11-creator <ofriki.omer@gmail.com>
2026-06-07 11:42:17 +0300

    Added all current files

---
+++ EndOfYearAlicecodeProjectOfriOmer2026/firstTry.js
@@ -0,0 +1,167 @@
+var areTheyLiked = {
+    "LikeButtonHeart1": false,
+    "LikeButtonHeart2": false,
+    "LikeButtonHeart3": false,
+    "LikeButtonHeart4": false,
+    "LikeButtonHeart5": false,
+    "LikeButtonHeart6": false,
+    "LikeButtonHeart7": false,
+    "LikeButtonHeart8": false,
+    "LikeButtonHeart9": false,
+    "LikeButtonHeart10": false,
+    "LikeButtonHeart11": false,
+    "LikeButtonHeart12": false,
+    "LikeButtonHeart13": false,
+    "LikeButtonHeart14": false,
+}
+
+function changeImage1() {
+    if (areTheyLiked.LikeButtonHeart1 == false) {
+        document.getElementById("LikeButtonHeart1").src = "HeartGraphicFull.png";
+        areTheyLiked.LikeButtonHeart1 = true
+    }
+    else {
+        document.getElementById("LikeButtonHeart1").src = "HeartGraphicNotFull.png";
+        areTheyLiked.LikeButtonHeart1 = false
+    }
+}
+function changeImage2() {
+    if (areTheyLiked.LikeButtonHeart2 == false) {
+        document.getElementById("LikeButtonHeart2").src = "HeartGraphicFull.png";
+        areTheyLiked.LikeButtonHeart2 = true
+    }
+    else {
+        document.getElementById("LikeButtonHeart2").src = "HeartGraphicNotFull.png";
+        areTheyLiked.LikeButtonHeart2 = false
+    }
+}
+function changeImage3() {
+    if (areTheyLiked.LikeButtonHeart3 == false) {
+        document.getElementById("LikeButtonHeart3").src = "HeartGraphicFull.png";
+        areTheyLiked.LikeButtonHeart3 = true
+    }
+    else {
+        document.getElementById("LikeButtonHeart3").src = "HeartGraphicNotFull.png";
+        areTheyLiked.LikeButtonHeart3 = false
+    }
+}
+function changeImage4() {
+    if (areTheyLiked.LikeButtonHeart4 == false) {
+        document.getElementById("LikeButtonHeart4").src = "HeartGraphicFull.png";
+        areTheyLiked.LikeButtonHeart4 = true
+    }
+    else {
+        document.getElementById("LikeButtonHeart4").src = "HeartGraphicNotFull.png";
+        areTheyLiked.LikeButtonHeart4 = false
+    }
+}
+function changeImage5() {
+    if (areTheyLiked.LikeButtonHeart5 == false) {
+        document.getElementById("LikeButtonHeart5").src = "HeartGraphicFull.png";
+        areTheyLiked.LikeButtonHeart5 = true
+    }
+    else {
+        document.getElementById("LikeButtonHeart5").src = "HeartGraphicNotFull.png";
+        areTheyLiked.LikeButtonHeart5 = false
+    }
+}
+function changeImage5() {
+    if (areTheyLiked.LikeButtonHeart5 == false) {
+        document.getElementById("LikeButtonHeart5").src = "HeartGraphicFull.png";
+        areTheyLiked.LikeButtonHeart5 = true
+    }
+    else {
+        document.getElementById("LikeButtonHeart5").src = "HeartGraphicNotFull.png";
+        areTheyLiked.LikeButtonHeart5 = false
+    }
+}
+function changeImage6() {
+    if (areTheyLiked.LikeButtonHeart6 == false) {
+        document.getElementById("LikeButtonHeart6").src = "HeartGraphicFull.png";
+        areTheyLiked.LikeButtonHeart6 = true
+    }
+    else {
+        document.getElementById("LikeButtonHeart6").src = "HeartGraphicNotFull.png";
+        areTheyLiked.LikeButtonHeart6 = false
+    }
+}
+function changeImage7() {
+    if (areTheyLiked.LikeButtonHeart7 == false) {
+        document.getElementById("LikeButtonHeart7").src = "HeartGraphicFull.png";
+        areTheyLiked.LikeButtonHeart7 = true
+    }
+    else {
+        document.getElementById("LikeButtonHeart7").src = "HeartGraphicNotFull.png";
+        areTheyLiked.LikeButtonHeart7 = false
+    }
+}
+function changeImage8() {
+    if (areTheyLiked.LikeButtonHeart8 == false) {
+        document.getElementById("LikeButtonHeart8").src = "HeartGraphicFull.png";
+        areTheyLiked.LikeButtonHeart8 = true
+    }
+    else {
+        document.getElementById("LikeButtonHeart8").src = "HeartGraphicNotFull.png";
+        areTheyLiked.LikeButtonHeart8 = false
+    }
+}
+function changeImage9() {
+    if (areTheyLiked.LikeButtonHeart9 == false) {
+        document.getElementById("LikeButtonHeart9").src = "HeartGraphicFull.png";
+        areTheyLiked.LikeButtonHeart9 = true
+    }
+    else {
+        document.getElementById("LikeButtonHeart9").src = "HeartGraphicNotFull.png";
+        areTheyLiked.LikeButtonHeart9 = false
+    }
+}
+function changeImage10() {
+    if (areTheyLiked.LikeButtonHeart10 == false) {
+        document.getElementById("LikeButtonHeart10").src = "HeartGraphicFull.png";
+        areTheyLiked.LikeButtonHeart10 = true
+    }
+    else {
+        document.getElementById("LikeButtonHeart10").src = "HeartGraphicNotFull.png";
+        areTheyLiked.LikeButtonHeart10 = false
+    }
+}
+function changeImage11() {
+    if (areTheyLiked.LikeButtonHeart11 == false) {
+        document.getElementById("LikeButtonHeart11").src = "HeartGraphicFull.png";
+        areTheyLiked.LikeButtonHeart11 = true
+    }
+    else {
+        document.getElementById("LikeButtonHeart11").src = "HeartGraphicNotFull.png";
+        areTheyLiked.LikeButtonHeart11 = false
+    }
+}
+function changeImage12() {
+    if (areTheyLiked.LikeButtonHeart12 == false) {
+        document.getElementById("LikeButtonHeart12").src = "HeartGraphicFull.png";
+        areTheyLiked.LikeButtonHeart12 = true
+    }
+    else {
+        document.getElementById("LikeButtonHeart12").src = "HeartGraphicNotFull.png";
+        areTheyLiked.LikeButtonHeart12 = false
+    }
+}
+function changeImage13() {
+    if (areTheyLiked.LikeButtonHeart13 == false) {
+        document.getElementById("LikeButtonHeart13").src = "HeartGraphicFull.png";
+        areTheyLiked.LikeButtonHeart13 = true
+    }
+    else {
+        document.getElementById("LikeButtonHeart13").src = "HeartGraphicNotFull.png";
+        areTheyLiked.LikeButtonHeart13 = false
+    }
+}
+function changeImage14() {
+    if (areTheyLiked.LikeButtonHeart14 == false) {
+        document.getElementById("LikeButtonHeart14").src = "HeartGraphicFull.png";
+        areTheyLiked.LikeButtonHeart14 = true
+    }
+    else {
+        document.getElementById("LikeButtonHeart14").src = "HeartGraphicNotFull.png";
+        areTheyLiked.LikeButtonHeart14 = false
+    }
+}
